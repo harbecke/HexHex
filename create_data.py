8a8f1ce99a293af0e2cb51665444f726cec92269
harbecke <d.harbecke@gmx.de>
2019-02-14 10:26:58 +0100

    create_data generates multiple files

--- create_data.py
+++ create_data.py
@@ -11,40 +11,46 @@ from hexgame import HexGame
 from hexconvolution import NoMCTSModel
 
 
-def generate_data_one_file(number_of_samples, model, device, run_name, noise, noise_level=0, temperature=1, board_size=11):
-    position_idx = 0
+def generate_data_files(number_of_files, samples_per_file, model, device, run_name, noise, noise_level=0, temperature=1, board_size=11):
     all_board_states = torch.Tensor()
     all_moves = torch.LongTensor()
     all_targets = torch.Tensor()
-    try:
-        os.makedirs("data")
-    except:
-        pass
-    while position_idx < number_of_samples:
-        board = Board(size=board_size)
-        hexgame = HexGame(board, model, device, noise, noise_level, temperature)
-        board_states, moves, targets = hexgame.play_moves()
-        print('played_game')
-        position_idx += len(board_states)
-        all_board_states = torch.cat((all_board_states,board_states))
-        all_moves = torch.cat((all_moves,moves))
-        all_targets = torch.cat((all_targets,targets))
-        print('created_game_data')
-        print(position_idx)
-    torch.save((all_board_states, all_moves, all_targets),f'data/{run_name}.pt')
+
+    file_counter = 0
+    while file_counter < number_of_files:
+        while all_board_states.shape[0] < samples_per_file:
+            
+            board = Board(size=board_size)
+            hexgame = HexGame(board, model, device, noise, noise_level, temperature)
+            board_states, moves, targets = hexgame.play_moves()
+
+            all_board_states = torch.cat((all_board_states,board_states))
+            all_moves = torch.cat((all_moves,moves))
+            all_targets = torch.cat((all_targets,targets))
+        
+        file_name = f'data/{run_name}_{file_counter}.pt'
+        torch.save((all_board_states[:samples_per_file], all_moves[:samples_per_file], all_targets[:samples_per_file]), file_name)
+        print(f'written_{file_name}')
+        file_counter += 1
+        
+        all_board_states = all_board_states[samples_per_file:]
+        all_moves = all_moves[samples_per_file:]
+        all_targets = all_targets[samples_per_file:]
 
 
 config = ConfigParser()
 config.read('config.ini')
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--board_size', type=int, default=config.get('CREATE DATA', 'board_size'))
+parser.add_argument('--number_of_files', type=int, default=config.get('CREATE DATA', 'number_of_files'))
+parser.add_argument('--samples_per_file', type=int, default=config.get('CREATE DATA', 'samples_per_file'))
 parser.add_argument('--model', type=str, default=config.get('CREATE DATA', 'model'))
-parser.add_argument('--positions_count', type=int, default=config.get('CREATE DATA', 'positions_count'))
 parser.add_argument('--run_name', type=str, default=config.get('CREATE DATA', 'run_name'))
 parser.add_argument('--noise_alpha', type=float, default=config.get('CREATE DATA', 'noise_alpha'))
 parser.add_argument('--noise_level', type=float, default=config.get('CREATE DATA', 'noise_level'))
 parser.add_argument('--temperature', type=float, default=config.get('CREATE DATA', 'temperature'))
+parser.add_argument('--board_size', type=int, default=config.get('CREATE DATA', 'board_size'))
+
 
 args = parser.parse_args()
 
@@ -52,4 +58,4 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = torch.load('models/{}.pt'.format(args.model))
 noise = Dirichlet(torch.full((args.board_size**2,), args.noise_alpha))
 
-generate_data_one_file(args.positions_count, model, device, args.run_name, noise, args.noise_level, args.temperature, args.board_size)
+generate_data_files(args.number_of_files, args.samples_per_file, model, device, args.run_name, noise, args.noise_level, args.temperature, args.board_size)
