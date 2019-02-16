81af944f40417d06cd673de7701b7649cc53510b
harbecke <d.harbecke@gmx.de>
2019-02-16 12:14:54 +0100

    added loading to device for all model loads

--- evaluate_on_data.py
+++ evaluate_on_data.py
@@ -15,7 +15,7 @@ args = parser.parse_args()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 criterion = nn.MSELoss(reduction='sum')
 
-model = torch.load('models/{}.pt'.format(args.model))
+model = torch.load('models/{}.pt'.format(args.model), map_location=device)
 model = model.to(device)
 
 board_tensor, moves_tensor, target_tensor = torch.load('data/{}.pt'.format(data))
