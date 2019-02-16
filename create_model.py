dcb9257de13504d365874709b3c1a5483b3945ad
Pascal Cremer <cremer@or.uni-bonn.de>
2019-02-16 23:14:40 +0100

    Make it easier to call train / create_model from other scripts

--- create_model.py
+++ create_model.py
@@ -16,13 +16,17 @@ def get_args(config_file):
 
     return parser.parse_args()
 
-def create_model(config_file = 'config.ini'):
+def create_model(args):
     print("=== creating model ===")
-    args = get_args(config_file)
-    model = NoMCTSModel(board_size=args.board_size, layers=args.layers, intermediate_channels=args.intermediate_channels)
+    model = NoMCTSModel(board_size=args.board_size, layers=args.layers,
+                        intermediate_channels=args.intermediate_channels)
     model_file = f'models/{args.model_name}.pt'
     torch.save(model, model_file)
     print(f'wrote {model_file}\n')
 
+def create_model_from_config_file(config_file):
+    args = get_args(config_file)
+    create_model(args)
+
 if __name__ == '__main__':
-    create_model()
+    create_model_from_config_file('config.ini')
