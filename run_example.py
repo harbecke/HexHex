afe85aafa97d025e4bac621b26fb04b7507af820
harbecke <d.harbecke@gmx.de>
2019-02-21 10:13:27 +0100

    fixed indentation in run_example.py

--- run_example.py
+++ run_example.py
@@ -7,10 +7,10 @@ import train
 
 
 def _main(config_file = 'sample_config.ini'):
-	'''
-	executes all basic scripts with values from sample config
-	should be run as test before every commit
-	'''
+    '''
+    executes all basic scripts with values from sample config
+    should be run as test before every commit
+    '''
     create_model.create_model_from_config_file(config_file)
     create_data.main(config_file)
     train.train_by_config_file(config_file)
