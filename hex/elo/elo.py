9c02f462746787b2a3a39f74be887005162d1f62
harbecke <d.harbecke@gmx.de>
2019-03-13 12:28:51 +0100

    implemented own elo calculation

--- hex/elo/elo.py
+++ hex/elo/elo.py
@@ -1,24 +1,65 @@
 #!/usr/bin/env python3
-import subprocess
+import itertools
+import torch
+import math
 
-from hex.elo import match
+import hex.utils.utils as utils
+from hex.evaluation import evaluate_two_models
 
 
-def export_tournament_as_pgn(filename, tournament):
-    with open(filename, 'w') as file:
-        for game in tournament:
-            file.write(game.to_pgn())
+def play_tournament(model_list):
 
-def create_ratings(tournament):
-    """needs to be called from hex home directory."""
-    export_tournament_as_pgn("game_history.pgn", tournament)
-    subprocess.check_call(['hex/elo/call_bayeselo.sh'])
+    num_models = len(model_list)
+    models = [torch.load('models/{}.pt'.format(model_file), map_location=utils.device) for model_file in model_list]
+    all_results = [[0 for _ in range(num_models)] for _ in range(num_models)]
 
-def test():
-    tournament = [
-        match.MatchResults("first model", "second model", [[0, 20], [10, 5]]),
-        match.MatchResults("first model", "third model", [[15, 5], [1, 29]]),
-        match.MatchResults("second model", "third model", [[10, 2], [16, 4]])
-    ]
-    create_ratings(tournament)
+    for first_idx, second_idx in itertools.combinations(range(num_models), 2):
+        result, signed_chi_squared = evaluate_two_models.play_games(
+                models=(models[first_idx], models[second_idx]),
+                device=utils.device,
+                openings=False,
+                number_of_games=32,
+                batch_size=32,
+                board_size=5,
+                temperature=0.1,
+                temperature_decay=0,
+                plot_board=False
+        )
+        all_results[first_idx][second_idx] = result[0][0] + result[1][0]
+        all_results[second_idx][first_idx] = result[0][1] + result[1][1]
 
+    return all_results
+
+
+def create_ratings(results, runs=100):
+    #from https://en.wikipedia.org/wiki/Bradley-Terry_model
+
+    num_models = len(results)
+    results_sum = [sum(result)+1 for result in results]
+    games = 2*sum(results_sum)/num_models
+    p_list = results_sum[:]
+
+    for _ in range(runs):
+        inverse_p_list = [[1/(p_list[idx1]+p_list[idx2]) for idx2 in range(num_models) if idx1!=idx2] for idx1 in range(num_models)]
+        sum_inverse_p_list = [1/sum(i) for i in inverse_p_list]
+        new_p_list = [results_sum[idx]/games*sum_inverse_p_list[idx] for idx in range(num_models)]
+        sum_p_list = sum(new_p_list)
+        p_list = [p/sum_p_list for p in new_p_list]
+
+    min_value = min(p_list)
+    elo_ratings = [math.log10(p/min_value)*400 for p in p_list]
+
+    return elo_ratings
+
+
+def output_ratings(model_list, output_file='ratings.txt'):
+    
+    tournament = play_tournament(model_list)
+    ratings = create_ratings(tournament)
+    model_with_ratings = list(zip(ratings, model_list))
+    model_with_ratings.sort(reverse=True)
+
+    with open(output_file, 'w') as file:
+        file.write('ELO\t\tModel\n')
+        for rating, model in model_with_ratings:
+            file.write('{}\t\t{}\n'.format(int(rating), model))
