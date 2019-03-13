ec9c22a385f55bcced83a1f5a4d74ba3bcccc2d7
harbecke <d.harbecke@gmx.de>
2019-03-13 14:47:39 +0100

    make lqloss into module

--- hex/utils/losses.py
+++ hex/utils/losses.py
@@ -1,16 +1,23 @@
 import torch
-from torch.nn import _reduction as _Reduction
+from torch._jit_internal import weak_module, weak_script_method
+from torch.nn.modules.loss import _Loss
 
-@torch._jit_internal.weak_script
-def lq_loss(input, target, q, reduction='mean'):
+@weak_module
+class LQLoss(_Loss):
     # type: (Tensor, Tensor, Optional[bool], Optional[bool], str) -> Tensor
     r"""lq_loss(input, target, q, reduction='mean') -> Tensor
 
     https://papers.nips.cc/paper/8094-generalized-cross-entropy-loss-for-training-deep-neural-networks-with-noisy-labels.pdf
     """
+    def __init__(self, q, reduction='mean'):
+        super(LQLoss, self).__init__(size_average=None, reduce=None, reduction=reduction)
+        self.q = q
 
-    ret = (1-(1-torch.abs(input-target))**q)/q
-    if reduction != 'none':
-        ret = torch.mean(ret) if reduction == 'mean' else torch.sum(ret)
+    @weak_script_method
+    def forward(self, input, target):
+        ret = (1-(1-torch.abs(input-target))**self.q)/self.q
+        if self.reduction != 'none':
+            ret = torch.mean(ret) if self.reduction == 'mean' else torch.sum(ret)
 
-    return ret
+        return ret
+
