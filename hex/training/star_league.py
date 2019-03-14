d01fcf522ba955b66c7a5e4c49dacb2db9997e9a
harbecke <d.harbecke@gmx.de>
2019-03-14 10:56:29 +0100

    implemented star_league

--- hex/training/star_league.py
+++ hex/training/star_league.py
@@ -5,34 +5,44 @@ import sys
 from configparser import ConfigParser
 
 import hex.training.train as train
+import hex.elo.elo as elo
 from hex.creation import create_data
 from hex.evaluation import evaluate_two_models
 
 
-def repeated_self_training(config_file, champions, runs, chi_squared_test_statistic):
+def league(config_file, champions, runs, chi_squared_test_statistic):
     """
     runs a self training loop
     each iteration produces a new model which is then trained on self-play data
-    data and model names are loop, so they take limited space and discard old data
+    data and model names are looped, so they take limited space and discard old data
     a model is preferred over the last if it performs significantly better according to a chi squared test
+    after a round of champions is trained a league is played between all current champions and old league winners
+    the new league winner becomes the initial champion for the next round
     """
     config = ConfigParser()
     config.read(config_file)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    champion_iter = 0
+    champion = torch.load(f'models/{config.get("CREATE DATA", "model")}.pt', map_location=device)
+    champion_filename = f'{config.get("SELF TRAINING", "champion_names")}0'
+    torch.save(champion, f'models/{champion_filename}.pt')
+
+    data_range_min = config.getint('CREATE DATA', 'data_range_min')
+    data_range_max = config.getint('CREATE DATA', 'data_range_max')
+    data_pos = data_range_min
+
+    champion_iter = 1
     champion_unbeaten_run = 0
-    champion_filename = config.get('CREATE DATA', 'model')
-    data_range_min=config.getint('CREATE DATA', 'data_range_min')
-    data_range_max=config.getint('CREATE DATA', 'data_range_max')
-    data_range=[data_range_min, data_range_min+1]
+    league_winners = 0
+    league_winner_elo = 0
+
     for run in range(runs):
         champion = torch.load(f'models/{champion_filename}.pt', map_location=device)
 
         create_data.generate_data_files(
-                file_counter_start=data_range[0],
-                file_counter_end=data_range[1],
+                file_counter_start=data_pos,
+                file_counter_end=data_pos+1,
                 samples_per_file=config.getint('CREATE DATA', 'samples_per_file'),
                 batch_size = config.getint('CREATE DATA', 'batch_size'),
                 model=champion,
@@ -44,19 +54,21 @@ def repeated_self_training(config_file, champions, runs, chi_squared_test_statis
                 temperature_decay=config.getfloat('CREATE DATA', 'temperature_decay'),
                 board_size=config.getint('CREATE DATA', 'board_size')
         )
-        new_data_range_max = data_range[1]+1
-        if data_range[1]+1 > data_range_max:
-            data_range=[data_range_min, data_range_min+1]
+
+        if data_pos+1 == data_range_max:
+            data_pos = data_range_min
         else:
-            data_range=[data_range[0]+1, data_range[1]+1]
+            data_pos += 1
 
         train_args = train.get_args(config_file)
-        new_model_name = f'{config.get("SELF TRAINING", "champion_names")}{champion_iter}'
+        champion_names = config.get("SELF TRAINING", "champion_names")
+        new_model_name = f'{champion_names}{champion_iter}'
 
         train_args.load_model = champion_filename
         train_args.save_model = new_model_name
         train.train(train_args)
         eval_args = evaluate_two_models.get_args(config_file)
+
         signed_chi_squared = evaluate_two_models.play_games(
                 models=(torch.load('models/'+new_model_name+'.pt'), champion),
                 openings=eval_args.openings,
@@ -70,14 +82,32 @@ def repeated_self_training(config_file, champions, runs, chi_squared_test_statis
 
         if signed_chi_squared > chi_squared_test_statistic:
             champion_unbeaten_run = 1
-            champion_filename = new_model_name
-            champion_iter = (champion_iter+1)%champions
-            print(f'Accept {champion_filename} as new champion!')
-        else:
-            if champion_unbeaten_run >= data_range_max-data_range_min:
-                champion_unbeaten_run = 1
+
+            if champion_iter+1 == champions:
+                champions_list = [f'{champion_names}{idx}' for idx in range(champions+league_winners)]
+                results = elo.play_tournament(champions_list)
+                ratings = elo.create_ratings(results)
+                champions_with_ratings = list(zip(ratings, champions_list))
+                if league_winners > 0:
+                    league_winner_elo -= champions_with_ratings[-1][0]
+                champions_with_ratings.sort(reverse=True)
+
+                league_winner_tuple = champions_with_ratings[0]
+                league_winner_elo += league_winner_tuple[0]
+                print(f'{league_winner_tuple[1]} won the league! It has ELO {league_winner_elo}!')
+                league_winner = torch.load(f'models/{league_winner_tuple[1]}.pt', map_location=device)
+                champion_filename = f'{champion_names}{champions+league_winners}'
+                torch.save(league_winner, f'models/{champion_filename}.pt')
+                league_winners += 1
+                champion_iter = 0
+
             else:
-                champion_unbeaten_run += 1
+                champion_iter += 1
+                champion_filename = new_model_name
+                print(f'Accept {champion_filename} as new champion!')
+
+        else:
+            champion_unbeaten_run += 1
             print(f'The champion remains in place, unbeaten for {champion_unbeaten_run} iterations. Iteration: {run+1}')
 
 def _main():
@@ -85,7 +115,10 @@ def _main():
     config = ConfigParser()
     config.read(config_file)
 
-    repeated_self_training(config_file, config.getint('SELF TRAINING', 'champions'), config.getint('SELF TRAINING', 'runs'), config.getfloat('SELF TRAINING', 'chi_squared_test_statistic'))
+    league(config_file,
+        config.getint('SELF TRAINING', 'champions'),
+        config.getint('SELF TRAINING', 'runs'),
+        config.getfloat('SELF TRAINING', 'chi_squared_test_statistic'))
 
 if __name__ == '__main__':
     _main()
