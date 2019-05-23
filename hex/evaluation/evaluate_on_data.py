318a9bca0ed0d6c6c9b3079324e9ccbeb6a7eb5e
harbecke <d.harbecke@gmx.de>
2019-05-23 15:10:45 +0200

    fix data loading and loss in evaluate_on_data

--- hex/evaluation/evaluate_on_data.py
+++ hex/evaluation/evaluate_on_data.py
@@ -15,16 +15,16 @@ parser.add_argument('--data', type=str, default=config.get('EVALUATE DATA', 'dat
 args = parser.parse_args()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-criterion = nn.MSELoss(reduction='sum')
+criterion = lambda pred, y: 0.8*nn.L1Loss(reduction='mean')(pred, y)+0.2*nn.BCELoss(reduction='mean')(pred, y)
 
 model, _ = load_model(f'models/{args.model}.pt')
 model = nn.DataParallel(model).to(device)
 
-board_tensor, moves_tensor, target_tensor = torch.load('data/{}.pt'.format(data))
+board_tensor, moves_tensor, target_tensor = torch.load('data/{}.pt'.format(args.data))
 board_tensor, moves_tensor, target_tensor = board_tensor.to(device), moves_tensor.to(device), target_tensor.to(device)
 
 with torch.no_grad():
-    full_pred_tensor = model(board_tensor)
+    full_pred_tensor = torch.exp(model(board_tensor))
     local_predictions = torch.gather(full_pred_tensor, 1, moves_tensor)
     loss = criterion(local_predictions.view(-1), target_tensor)
-    print(loss)
+    print(loss.item())
