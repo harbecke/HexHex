9a5a01c8a9555ec0e3403ec4fbc90e746d7a0038
harbecke <d.harbecke@gmx.de>
2019-05-28 18:59:37 +0200

    validation with logarithmic prediction

--- hex/training/train.py
+++ hex/training/train.py
@@ -188,7 +188,7 @@ def train_model(model, save_model_path, dataloader, criterion, optimizer, epochs
 
                 if validation_triple is not None:
                     with torch.no_grad():
-                        val_pred_tensor = model(validation_triple[0])
+                        val_pred_tensor = torch.exp(model(validation_triple[0]))
                         val_values = torch.gather(val_pred_tensor, 1, validation_triple[1])
                         val_loss = criterion(val_values.view(-1), validation_triple[2])
 
