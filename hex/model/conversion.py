336aa141e2c79afe0b6d8c333c8721b3b110de23
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2019-07-15 15:08:02 +0200

    Update converter for new model syntax

--- hex/model/conversion.py
+++ hex/model/conversion.py
@@ -1,28 +1,29 @@
-import torch
 import sys
+from configparser import ConfigParser
 
-from hex.creation.create_model import get_args, create_model
+import torch
+
+from hex.creation.create_model import create_and_store_model
 from hex.utils.utils import device
 
-def convert_model(config, old_model_file):
-    args = get_args(config)
-    new_model = create_model(args)
-    old_model = torch.load(old_model_file, map_location=device)
 
-    new_model_file = f'models/{args.model_name}.pt'
-    torch.save({
-        'model_state_dict': old_model.state_dict(),
-        'board_size': args.board_size,
-        'model_type': args.model_type,
-        'layers': args.layers,
-        'layer_type': args.layer_type,
-        'intermediate_channels': args.intermediate_channels,
-        'optimizer': False
-        }, new_model_file)
+def convert_model(model_name):
+    old_model = torch.load(f'models/{model_name}.pt', map_location=device)
+    config = ConfigParser()
+    config = config['DEFAULT']
+    for key, val in old_model.items():
+        if key != 'model_state_dict':
+            config[key] = str(val)
+    config['model_name'] = model_name
+    create_and_store_model(config)
     print('=== converted model ===')
-    print(f'wrote {new_model_file}\n')
     return
 
+
+def _main():
+    old_model_name = sys.argv[1]
+    convert_model(old_model_name)
+
+
 if __name__ == '__main__':
-    old_model_file = sys.argv[1]
-    convert_model('config.ini', old_model_file)
+    _main()
