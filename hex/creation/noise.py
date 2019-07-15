e3eca5940a3abc133552922d5940e349c8ea23df
harbecke <d.harbecke@gmx.de>
2019-07-15 17:45:55 +0200

    modify singh_maddala noise

--- hex/creation/noise.py
+++ hex/creation/noise.py
@@ -4,13 +4,13 @@ from torch.distributions.pareto import Pareto
 
 def singh_maddala_onto_output(output_tensor, noise_alpha, noise_beta, noise_lambda, device):
     '''
-    one value of the output_tensor gets multplied with (1 + a sampled value of singh_maddala)
+    one value of the output_tensor gets increased by a sampled value of singh_maddala
     https://en.wikipedia.org/wiki/Burr_distribution
     alpha=k, beta=c
     '''
     batch_size = output_tensor.shape[0]
     output_tensor[torch.arange(0, batch_size).long(), torch.randint(torch.numel(output_tensor[0]), 
-        (batch_size,))] *= 1 + noise_lambda*(Pareto(1, noise_alpha).sample((batch_size,)).to(device)-1)**(1/noise_beta)
+        (batch_size,))] += noise_lambda*(Pareto(1, noise_alpha).sample((batch_size,)).to(device)-1)**(1/noise_beta)
     return output_tensor
 
 
