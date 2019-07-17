c9e542decf0e984387f759a0b89a3bf6b7a3485d
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2019-07-17 18:31:34 +0200

    Add val/train loss tensorboard plot

--- hex/utils/summary.py
+++ hex/utils/summary.py
@@ -1,3 +1,6 @@
 from tensorboardX import SummaryWriter
 
 writer = SummaryWriter(flush_secs=5)
+
+layout = {'training': {'val/train loss': ['Multiline', ['train/train_loss', 'train/val_loss']]}}
+writer.add_custom_scalars(layout)
