95ef493fddb856a3c3df330dcdd7b7999711b13b
harbecke <d.harbecke@gmx.de>
2019-08-19 18:24:25 +0200

    rewrite BayesianOptimization

--- hex/training/bayesian_optimization.py
+++ hex/training/bayesian_optimization.py
@@ -5,38 +5,49 @@ from ax.service.managed_loop import optimize
 from hex.training.repeated_self_training import RepeatedSelfTrainer
 
 
-def train_evaluate(parameters):
-    trainer = RepeatedSelfTrainer("config.ini")
+class BayesianOptimization:
+    def __init__(self, parameters):
+        self.parameters = parameters
 
-    for key, value in parameters.items():
-        trainer.config["TRAIN"][key] = str(value)
-    
-    trainer.repeated_self_training()
-    #p = multiprocessing.Process(target=trainer.repeated_self_training())   
-    #p.start()
+    def train_evaluate(self):
+        trainer = RepeatedSelfTrainer("config.ini")
 
-    # Wait for 10 seconds or until process finishes
-    #p.join(10)
+        for key, value in parameters.items():
+            section = next(parameter["section"] for parameter in self.parameters if parameter["name"] == key)
+            trainer.config[self.parameters[key][section]][key] = str(value)
 
-    # If thread is still active
-    #if p.is_alive():
+        trainer.repeated_self_training()
+        #p = multiprocessing.Process(target=trainer.repeated_self_training())
+        #p.start()
 
-        # Terminate
-        #p.terminate()
-        #p.join()
+        # Wait for 10 seconds or until process finishes
+        #p.join(10)
 
-    return trainer.get_best_rating()
+        # If thread is still active
+        #if p.is_alive():
+
+            # Terminate
+            #p.terminate()
+            #p.join()
+        return trainer.get_best_rating()
+
+    def optimizer_run(self):
+        best_parameters, values, experiment, model = optimize(
+            parameters=self.parameters,
+            evaluation_function=self.train_evaluate,
+            objective_name='elo'
+        )
+        return best_parameters, values, experiment, model
+
 
 if __name__ == '__main__':
     parameters = [
             {"name": "learning_rate", "type": "range", "bounds": [1e-6, 1e-3], "log_scale": True, "section": "TRAIN"},
             {"name": "weight_decay", "type": "range", "bounds": [1e-8, 1.0], "log_scale": True, "section": "TRAIN"}
         ]
-    best_parameters, values, experiment, model = optimize(
-        parameters=parameters,
-        evaluation_function=train_evaluate,
-        objective_name='elo'
-    )
+    bo = BayesianOptimization(parameters)
+    best_parameters, values, experiment, model = bo.optimizer_run()
+
     print(best_parameters)
     means, covariances = values
     print(means, covariances)
