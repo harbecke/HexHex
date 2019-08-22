524a10ced60df05c46d56554b223e89225f1c91d
harbecke <d.harbecke@gmx.de>
2019-08-22 17:09:09 +0200

    changing Bayesian Optimization framework

--- hex/training/bayesian_optimization.py
+++ hex/training/bayesian_optimization.py
@@ -3,8 +3,7 @@ import json
 import os
 import pickle
 from configparser import ConfigParser
-import ax
-from ax.service.managed_loop import optimize
+from bayes_opt import BayesianOptimization, observer, event
 
 from hex.training.repeated_self_training import RepeatedSelfTrainer, load_reference_models
 from hex.utils.logger import logger
@@ -12,24 +11,26 @@ from hex.utils.logger import logger
 
 class BayesianOptimization:
     """
-    runs Bayesian Optimization with given parameters and 20 steps
-    optimizes by ELO value compared to starting model
+    runs Bayesian Optimization with given parameters and "loop_count" steps
+    optimizes by ELO value compared to starting and reference models
+    alpha value "explained":
+    https://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpr_noisy.html
     """
     def __init__(self, parameters, config):
         self.parameters = parameters
+        self.bounds = dict([(parameter, pdict["bounds"]) for parameter, pdict in self.parameters.items()])
         self.config = config
         self.reference_models = load_reference_models(config)
+        self.optimizer = BayesianOptimization(f=self.train_evaluate(), pbounds=self.bounds)
 
-    def train_evaluate(self, parameters):
+    def train_evaluate(self):
         start_time = time.time()
         trainer = RepeatedSelfTrainer(self.config)
         trainer.reference_models = self.reference_models
 
-        for parameter_name, value in parameters.items():
-            logger.info(f"Bayesian Optimization {parameter_name}: {value}")
-            section = next(parameter["section"] for parameter in self.parameters
-                if parameter["name"] == parameter_name)
-            trainer.config[section][parameter_name] = str(value)
+        for parameter, pdict in self.parameters.items():
+            logger.info(f"Bayesian Optimization {parameter}: {pdict}")
+            trainer.config[pdict["section"]][parameter] = str(3)
         epochs = trainer.config["TRAIN"].getfloat("epochs")
 
         trainer.prepare_rst()
@@ -44,14 +45,11 @@ class BayesianOptimization:
 
         return trainer.get_best_rating()
 
-    def optimizer_run(self):
-        best_parameters, values, experiment, model = optimize(
-            parameters=self.parameters,
-            evaluation_function=self.train_evaluate,
-            objective_name='elo',
-            total_trials=self.config["BAYESIAN OPTIMIZATION"].getint("loop_count")
-        )
-        return best_parameters, values, experiment, model
+    def optimizer_run(self, name):
+        logger = observer.JSONLogger(path=f"bayes_experiments/{name}.json")
+        self.optimizer.subscribe(event.Events.OPTMIZATION_STEP, logger)
+        self.optimizer.maximize(n_iter=self.config["BAYESIAN OPTIMIZATION"].getint("loop_count"),
+            alpha=self.config["BAYESIAN OPTIMIZATION"].getfloat("alpha"))
 
 
 def main():
@@ -72,15 +70,10 @@ def main():
     bo = BayesianOptimization(parameters, config)
 
     try:
-        best_parameters, values, experiment, model = bo.optimizer_run()
+        bo.optimizer_run(config["CREATE MODEL"].get("model_name"))
 
         logger.info("=== best parameters are ===")
-        logger.info(best_parameters)
-
-        experiment.name = config["CREATE MODEL"].get("model_name")
-        with open(f"bayes_experiments/{experiment.name}_parameters.p", "wb") as file:
-            pickle.dump((best_parameters, values), file)
-        ax.save(experiment, f"bayes_experiments/{experiment.name}_experiment.json")
+        logger.info(bo.optimizer.max)
 
     except KeyboardInterrupt:
         print("Shutdown requested...exiting")
