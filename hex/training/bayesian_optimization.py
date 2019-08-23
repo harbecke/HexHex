4fd422a2c8114c1330fb9d5504de2a63bc0fd1e9
harbecke <d.harbecke@gmx.de>
2019-08-23 16:28:27 +0200

    change Bayesian Optimization framework to skopt

--- hex/training/bayesian_optimization.py
+++ hex/training/bayesian_optimization.py
@@ -3,56 +3,30 @@ import json
 import os
 import pickle
 from configparser import ConfigParser
-from bayes_opt import BayesianOptimization, observer, event
+from skopt import gp_minimize
+from skopt.space import Real, Integer
+from skopt.utils import use_named_args
 
 from hex.training.repeated_self_training import RepeatedSelfTrainer, load_reference_models
 from hex.utils.logger import logger
 
 
-class BayesianOptimization:
-    """
-    runs Bayesian Optimization with given parameters and "loop_count" steps
-    optimizes by ELO value compared to starting and reference models
-    alpha value "explained":
-    https://scikit-learn.org/stable/auto_examples/gaussian_process/plot_gpr_noisy.html
-    """
-    def __init__(self, parameters, config):
-        self.parameters = parameters
-        self.bounds = dict([(parameter, pdict["bounds"]) for parameter, pdict in self.parameters.items()])
-        self.config = config
-        self.reference_models = load_reference_models(config)
-        self.optimizer = BayesianOptimization(f=self.train_evaluate(), pbounds=self.bounds)
+def parameter_dict_to_named_arg(pdict):
+    low, high = pdict["bounds"]
+    if type(low) == type(high):
+        if type(low) == float:
+            prior = "log-uniform" if pdict.get("log_scale") else "uniform"
+            return Real(low=low, high=high, prior=prior, name=pdict["name"])
+        elif type(low) == int:
+            return Int(low=low, high=high, name=pdict["name"])
+    else:
+        logger.info(f"=== parameter {pdict['name']} doesn't match (known) types ===")
+        raise SystemExit
 
-    def train_evaluate(self):
-        start_time = time.time()
-        trainer = RepeatedSelfTrainer(self.config)
-        trainer.reference_models = self.reference_models
+def bayesian_optimization():
+    #runs Bayesian Optimization with given parameters and "loop_count" steps
+    #optimizes by ELO value compared to starting and reference models
 
-        for parameter, pdict in self.parameters.items():
-            logger.info(f"Bayesian Optimization {parameter}: {pdict}")
-            trainer.config[pdict["section"]][parameter] = str(3)
-        epochs = trainer.config["TRAIN"].getfloat("epochs")
-
-        trainer.prepare_rst()
-        loop_idx = self.config.getint('REPEATED SELF TRAINING', 'start_index') + 1
-
-        while True:
-            if time.time() - start_time < self.config["BAYESIAN OPTIMIZATION"].getfloat("loop_time"):
-                trainer.rst_loop(loop_idx)
-                loop_idx += 1
-            else:
-                break
-
-        return trainer.get_best_rating()
-
-    def optimizer_run(self, name):
-        logger = observer.JSONLogger(path=f"bayes_experiments/{name}.json")
-        self.optimizer.subscribe(event.Events.OPTMIZATION_STEP, logger)
-        self.optimizer.maximize(n_iter=self.config["BAYESIAN OPTIMIZATION"].getint("loop_count"),
-            alpha=self.config["BAYESIAN OPTIMIZATION"].getfloat("alpha"))
-
-
-def main():
     parameters_path = "bo_parameters.json"
     if not os.path.isfile(parameters_path):
         with open(parameters_path, 'w') as file:
@@ -67,19 +41,45 @@ def main():
 
     config = ConfigParser()
     config.read('config.ini')
-    bo = BayesianOptimization(parameters, config)
+    reference_models = load_reference_models(config)
+    space = [parameter_dict_to_named_arg(pdict) for pdict in parameters]
 
-    try:
-        bo.optimizer_run(config["CREATE MODEL"].get("model_name"))
+    @use_named_args(space)
+    def train_evaluate(**params):
+        trainer = RepeatedSelfTrainer(config)
+        trainer.reference_models = reference_models
 
-        logger.info("=== best parameters are ===")
-        logger.info(bo.optimizer.max)
+        start_time = time.time()
+        for parameter_name, value in params.items():
+            logger.info(f"Bayesian Optimization {parameter_name}: {value}")
+            section = next(parameter["section"] for parameter in parameters
+                if parameter["name"] == parameter_name)
+            trainer.config[section][parameter_name] = str(value)
+        epochs = trainer.config["TRAIN"].getfloat("epochs")
 
-    except KeyboardInterrupt:
-        print("Shutdown requested...exiting")
+        trainer.prepare_rst()
+        loop_idx = config.getint('REPEATED SELF TRAINING', 'start_index') + 1
 
-    raise SystemExit
+        while True:
+            if time.time() - start_time < config["BAYESIAN OPTIMIZATION"].getfloat("loop_time"):
+                trainer.rst_loop(loop_idx)
+                loop_idx += 1
+            else:
+                break
+
+        return trainer.get_best_rating()
+
+    res_gp = gp_minimize(
+        func=train_evaluate,
+        dimensions=space,
+        n_calls=config["BAYESIAN OPTIMIZATION"].getint("loop_count"),
+        n_random_starts=config["BAYESIAN OPTIMIZATION"].getint("random_count"),
+        noise=config["BAYESIAN OPTIMIZATION"].getfloat("noise")
+        )
+
+    logger.info("=== best parameters are ===")
+    logger.info(res_gp.x, res_gp.fun)
 
 
 if __name__ == '__main__':
-    main()
+    bayesian_optimization()
