3fe4b95118a0a504eba9502d84db000be91cd4b9
harbecke <d.harbecke@gmx.de>
2019-08-26 12:58:58 +0200

    remove model_type from conversion_size

--- hex/model/conversion_size.py
+++ hex/model/conversion_size.py
@@ -6,26 +6,22 @@ from hex.utils.utils import device
 def convert_boardsize_of_model(model_name, new_bs):
     checkpoint = torch.load(f'models/{model_name}.pt', map_location=device)
     config = checkpoint['config']
-    if config['model_type'] == 'conv':
-        config['board_size'] = new_bs
+    config['board_size'] = new_bs
 
-        bias_key = 'bias'
-        while True:
-            if bias_key in checkpoint['model_state_dict']:
-                checkpoint['model_state_dict'][bias_key] = torch.zeros(int(new_bs)**2)
-                break
-            bias_key = 'internal_model.' + bias_key
+    bias_key = 'bias'
+    while True:
+        if bias_key in checkpoint['model_state_dict']:
+            checkpoint['model_state_dict'][bias_key] = torch.zeros(int(new_bs)**2)
+            break
+        bias_key = 'internal_model.' + bias_key
 
-        torch.save({
-        'model_state_dict': checkpoint['model_state_dict'],
-        'config': config,
-        'optimizer': False
-        }, f'models/{new_bs}_{model_name}.pt')
-        print('=== converted model size ===')
-        print(f'wrote models/{new_bs}_{model_name}.pt')
-    else:
-        print('=== model type is not "conv" ===')
-    return
+    torch.save({
+    'model_state_dict': checkpoint['model_state_dict'],
+    'config': config,
+    'optimizer': False
+    }, f'models/{new_bs}_{model_name}.pt')
+    print('=== converted model size ===')
+    print(f'wrote models/{new_bs}_{model_name}.pt')
 
 if __name__ == '__main__':
     old_model_name = sys.argv[1]
