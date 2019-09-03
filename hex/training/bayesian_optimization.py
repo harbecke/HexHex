e9841db3dff1c9065e7cc27b4ed5bb0f80dcdfbd
harbecke <d.harbecke@gmx.de>
2019-09-03 12:38:06 +0200

    increase cpu load for bayesian optimization

--- hex/training/bayesian_optimization.py
+++ hex/training/bayesian_optimization.py
@@ -91,7 +91,8 @@ def bayesian_optimization():
         y0 = y0,
         callback=checkpoint_callback,
         n_restarts_optimizer=config["BAYESIAN OPTIMIZATION"].getint("optimizer_restarts"),
-        noise=config["BAYESIAN OPTIMIZATION"].getfloat("noise")
+        noise=config["BAYESIAN OPTIMIZATION"].getfloat("noise"),
+        n_jobs=4
         )
 
     logger.info("")
