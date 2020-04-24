300c08fea1d22c9cb0fd52804bca323d78aba1f1
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2020-04-24 23:17:13 +0200

    Fix rotation of model input/output

--- app/src/App.js
+++ app/src/App.js
@@ -15,6 +15,7 @@ const url = "./11_2w4_1100.onnx";
 let sess_init = false;
 let info = "";
 let agent_is_blue = true;
+let max_rating;
 
 async function load_model() {
   await sess.loadModel(url);
@@ -74,13 +75,14 @@ function AddStone(G, id, player_color) {
 
   G.connected_sets[player_color].push(Array.from(new_cc));
   G.connected_set_rows[player_color].push(Array.from(new_cc_rows));
+  G.last_move = id;
   return new_cc_rows.has(0) && new_cc_rows.has(board_size - 1);
 }
 
 class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {showRatings: props.initial};
+    this.state = { showRatings: props.initial };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
@@ -88,15 +90,15 @@ class Toggle extends React.Component {
 
   handleClick() {
     this.props.toggle(!this.state.showRatings);
-    this.setState(state => ({
-      showRatings: !state.showRatings
+    this.setState((state) => ({
+      showRatings: !state.showRatings,
     }));
   }
 
   render() {
     return (
       <button onClick={this.handleClick}>
-        {this.state.showRatings ? 'Hide Ratings' : 'Show Ratings'}
+        {this.state.showRatings ? "Hide Ratings" : "Show Ratings"}
       </button>
     );
   }
@@ -109,6 +111,7 @@ const HexGame = {
     connected_set_rows: [[], []],
     winner: null,
     model_output: Array(board_size * board_size).fill(null),
+    model_display: Array(board_size * board_size).fill(null),
     // index 0 will always be red independent of swap
     // save a pair of sets for each player:
     //   * the first set is a connected component of stones
@@ -120,16 +123,14 @@ const HexGame = {
   moves: {
     clickCell: (G, ctx, id) => {
       const num_moves = G.cells.reduce((x, y) => x + (y ? 1 : 0), 0);
-      if (num_moves == 1 && G.cells[id] == '0') {
+      if (num_moves == 1 && G.cells[id] == "0") {
         // switch!
         agent_is_blue = !agent_is_blue;
-        info = "Switched!"
-      }
-
-      else {
+        info = "Switched!";
+      } else {
         let cur_player = ctx.currentPlayer;
         if (!agent_is_blue) {
-          cur_player = ctx.currentPlayer === '0' ? '1' : '0';
+          cur_player = ctx.currentPlayer === "0" ? "1" : "0";
         }
         G.cells[id] = cur_player;
         const has_won = AddStone(G, id, cur_player);
@@ -167,7 +168,7 @@ class HexBoard extends React.Component {
   constructor(props) {
     super(props);
     this.setDisplayRatings = this.setDisplayRatings.bind(this);
-    this.state = {display_ratings: false};
+    this.state = { display_ratings: false };
   }
 
   onClick(id) {
@@ -187,21 +188,37 @@ class HexBoard extends React.Component {
       // AI move selection
       const num_moves = this.props.G.cells.reduce((x, y) => x + (y ? 1 : 0), 0);
       let best = -1;
-      let best_value;
       for (let i = 0; i < board_size * board_size; i++) {
         if (num_moves <= 1 || this.props.G.cells[i] === null) {
-          if (best === -1 || result[i] > best_value) {
+          if (best === -1 || result[i] > max_rating) {
             best = i;
-            best_value = result[i];
+            max_rating = result[i];
           }
         }
       }
+      let score_sum = 0;
+      for (let i = 0; i < board_size * board_size; i++) {
+        if (num_moves <= 1 || this.props.G.cells[i] === null) {
+          const score = Math.pow(2, result[i] - max_rating);
+          score_sum += score;
+        }
+      }
+
+      for (let i = 0; i < board_size * board_size; i++) {
+        if (num_moves <= 1 || this.props.G.cells[i] === null) {
+          const score = Math.pow(2, result[i] - max_rating);
+          this.props.G.model_display[i] = (100 * score / score_sum).toFixed(0);
+        } else {
+          this.props.G.model_display[i] = "";
+        }
+      }
+
       this.props.moves.clickCell(best);
     });
   }
 
   setDisplayRatings(display_ratings) {
-    this.setState({display_ratings: display_ratings});
+    this.setState({ display_ratings: display_ratings });
   }
 
   async runModel(cells) {
@@ -239,17 +256,33 @@ class HexBoard extends React.Component {
           }
         }
       }
+      let input_values2 = [];
+      for (let id = 0; id < board_size * board_size; id++) {
+        input_values2.push(input_values[board_size*board_size - id - 1]);
+      }
+      for (let id = 0; id < board_size * board_size; id++) {
+        input_values2.push(input_values[2*board_size*board_size - id - 1]);
+      }
       const input = [
         new Tensor(new Float32Array(input_values), "float32", [1, 2, 11, 11]),
       ];
+      const input2 = [
+        new Tensor(new Float32Array(input_values2), "float32", [1, 2, 11, 11]),
+      ];
       const output = await sess.run(input);
+      const output2 = await sess.run(input2);
       const outputTensor = output.values().next().value;
+      const outputTensor2 = output2.values().next().value;
+      console.log(outputTensor2);
       let output_transposed = [];
-      if (agent_is_blue){
+      if (agent_is_blue) {
         for (let x = 0; x < board_size; x++) {
           for (let y = 0; y < board_size; y++) {
             const id = PosToId(x, y);
-            output_transposed.push(outputTensor.data[id]);
+            output_transposed.push(
+              (outputTensor.data[id] +
+               outputTensor2.data[board_size * board_size - id - 1]
+              ) / 2);
           }
         }
       } else {
@@ -259,6 +292,7 @@ class HexBoard extends React.Component {
       for (let i = 0; i < board_size * board_size; i++) {
         this.props.G.model_output[i] = output_transposed[i];
       }
+
       info = "";
       return output_transposed;
     } catch (e) {
@@ -277,13 +311,7 @@ class HexBoard extends React.Component {
   }
 
   cellText(id, display_ratings) {
-    if (display_ratings && this.props.G.model_output[0] !== null) {
-      const num_moves = this.props.G.cells.reduce((x, y) => x + (y ? 1 : 0), 0);
-      if (num_moves <= 1 || this.props.G.cells[id] === null) {
-        return this.props.G.model_output[id].toFixed(1);
-      }
-    }
-    return "";
+    return this.props.G.model_display[id];
   }
 
   isActive(id) {
@@ -294,8 +322,11 @@ class HexBoard extends React.Component {
 
   render() {
     if (this.props.ctx.gameover) {
-      let player = agent_is_blue ? '0' : '1';
-      info = this.props.ctx.gameover.winner === player ? 'Player has won!' : 'Agent has won!';
+      let player = agent_is_blue ? "0" : "1";
+      info =
+        this.props.ctx.gameover.winner === player
+          ? "Player has won!"
+          : "Agent has won!";
     }
 
     const p1Style = {
@@ -363,13 +394,14 @@ class HexBoard extends React.Component {
     return (
       <div>
         <div class="intro">
-          HexHex - A reinforcement deep learning agent by Simon Buchholz, David Harbecke, and Pascal Van Cleeff.
+          HexHex - A reinforcement deep learning agent by Simon Buchholz, David
+          Harbecke, and Pascal Van Cleeff.
           <br />
           <a href="https://github.com/harbecke/hex">github.com/harbecke/hex</a>
         </div>
         <div id="winner">{info}</div>
         <div id="controls">
-          <Toggle initial={false} toggle={this.setDisplayRatings}/>
+          <Toggle initial={false} toggle={this.setDisplayRatings} />
         </div>
         <div className="App">
           <HexGrid width={1000} height={800} viewBox="0 -3 30 30">
