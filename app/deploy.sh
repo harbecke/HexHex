c560a86ce2704e0b1246aa8f7ca0b7bd22145467
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2020-04-26 19:08:30 +0200

    Update deploy script

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# This will deploy the app to cleeff.github.io from Pascal's machine :-)
+
 npm run build
 rm -r ~/code/cleeff.github.io/hex
 cp -r build ~/code/cleeff.github.io/hex
+cd ~/code/cleeff.github.io 
+git add -A && git commit -m "Update hex" && git push
+
+
+
