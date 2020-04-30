3a48e6f45a6e0c400b31e4f7d8711bd61b9e3b28
Pascal Van Cleeff <pascal.van.cleeff@gmail.com>
2020-04-30 19:16:40 +0200

    Fix onnx export

--- hexhex/export/onnx_export.py
+++ hexhex/export/onnx_export.py
@@ -7,7 +7,7 @@ def main(model_name=None):
     if model_name is None:
         model_name = '11_2w4_2000'
     model = load_model(f"models/{model_name}.pt", export_mode=True)
-    dummy_input = torch.zeros(1, 2, model.board_size, model.board_size)
+    dummy_input = torch.zeros(1, 2, model.board_size + 2, model.board_size + 2)
     torch.onnx.export(model, dummy_input, f'{model_name}.onnx', verbose=True)
 
 
